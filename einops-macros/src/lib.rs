cad52aa6a021b6cbff5b538b2e67123ed4f1e1a9
VasanthakumarV <vasanth260m12@gmail.com>
2022-06-03 18:36:00 +0530

    Add user doc for the macro

--- einops-macros/src/lib.rs
+++ einops-macros/src/lib.rs
@@ -1,5 +1,12 @@
 mod einops;
 
+/// Macro to perform tensor transformations using simple expressions
+///
+/// # Example
+///
+/// ```no_run
+/// let output = einops!("h w c -> c h w", &input);
+/// ```
 #[proc_macro]
 pub fn einops(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     einops::einops(input.into())
